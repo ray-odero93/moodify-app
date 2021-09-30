de3577a408c9b6af071b90a25af495d8b1aab0d7
Ondigo Wendy <wendy.ondigo@student.moringaschool.com>
2021-09-30 12:20:54 +0300

    Change background layout

--- src/main/java/models/Database.java
+++ src/main/java/models/Database.java
@@ -3,12 +3,7 @@ package models;
 import org.sql2o.Sql2o;
 
 public class Database {
-
-//  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker"," moringa", "Access1");
-//    ////To run app locally
+//    To run app locally
      public static String connectionString = "jdbc:postgresql://localhost:5432/moodify";
   public static    Sql2o sql2o = new Sql2o(connectionString, "moringa", "Access1");
-   // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-44-198-223-154.compute-1.amazonaws.com:5432/daa7t7lmet2sou?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "zmpkonnizwzucd","c795ec211f480b77e7d471dbf87206d4dce5b428b0f59f062fb4da72c5bda9f3");
-
-
 }
