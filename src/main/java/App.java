954ef6748aceb2f94a0ac7635d9984172e36cf93
Esther-Moki <esther.moki@student.moringaschool.com>
2021-10-01 07:34:03 +0300

    added the connection string to app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,22 @@
+import dao.Sql2oLoginDao;
+import dao.Sql2oUsersDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 public class App {
+    public static void main(String[] args) {
+        Sql2oUsersDao usersDao;
+        Sql2oLoginDao LoginDao;
+        Connection conn;
+
+
+        String connectionString = "jdbc:h2:~/users_information.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        usersDao = new Sql2oUsersDao(sql2o);
+        LoginDao = new Sql2oLoginDao(sql2o);
+        conn = sql2o.open();
+
+
+    }
+
 }
